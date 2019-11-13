6e4ffcc09dc0074bfc21acc868b9b84aaf4def3c
Yannick Miñano <leoynick@gmail.com>
2019-11-13 13:07:43 +0100

    Add horizontal separator

--- src/videos/containers/category-list.js
+++ src/videos/containers/category-list.js
@@ -1,26 +1,28 @@
 import React from 'react'
 import { FlatList } from 'react-native'
 
+import Category from '../components/category'
 import Empty from '../components/empty'
-import Separator from '../components/vertical-separator'
-import Suggestion from '../components/suggestion'
+import Layout from '../components/category-list-layout'
+import Separator from '../../sections/components/horizontal-separator'
 
 const renderEmpty = () => <Empty text='No hay ningun elemento' />
 const itemSeparator = () => <Separator />
 const keyExtractor = item => item.id.toString()
 
-export const CategoryList = ({ list, genres }) => {
-  const renderSuggestion = ({ item }) => <Suggestion {...item} genres={genres} />
+export const CategoryList = ({ genres }) => {
+  const renderCategory = ({ item }) => <Category {...item} />
   return (
-    <FlatList
-      horizontal
-      data={list}
-      genres={genres}
-      ItemSeparatorComponent={itemSeparator}
-      keyExtractor={keyExtractor}
-      ListEmptyComponent={renderEmpty}
-      renderItem={renderSuggestion}
-    />
+    <Layout title='Categorías'>
+      <FlatList
+        horizontal
+        data={genres}
+        ItemSeparatorComponent={itemSeparator}
+        keyExtractor={keyExtractor}
+        ListEmptyComponent={renderEmpty}
+        renderItem={renderCategory}
+      />
+    </Layout>
   )
 }
 
