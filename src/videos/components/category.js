83200333a414fd771894b22201ec87160e204ca6
Yannick Miñano <leoynick@gmail.com>
2019-11-13 13:29:35 +0100

    Add styles to category component

--- src/videos/components/category.js
+++ src/videos/components/category.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function (props) {
   return (
-    <View>
-      <Text>{props.name}</Text>
+    <View style={styles.wrapper}>
+      <Text style={styles.genre}>{props.name}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'grey',
+    opacity: 0.8,
+    width: 250,
+    height: 100,
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  genre: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0,0,0,.75)',
+    textShadowOffset: {
+      width: 2,
+      height: 2
+    },
+    textShadowRadius: 10
+
+  }
+})
